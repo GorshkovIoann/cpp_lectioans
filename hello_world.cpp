773259b9c72197cb24971eaaec00f4fa0c9b605d
GorshkovIoann <ioann.gorshkoffo@mail.ru>
2023-09-09 09:30:27 +0300

    first try

---
+++ hello_world.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+int main()
+{
+    int n;
+    while(cin>> n){
+        if (n>10)
+            simple_error("really?");
+        else 
+            cout<<n;
+
+
+
+
+    }
+    
+    
+    return 0;
+}
---
+++ pr1.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+
+
+int main()
+{
+    string main_menu = "        Hello, player\n You are welcome to the game NIM\nYou should to be ready: your opponent really strong\n For start game print 1 + enter\n For options print 2 + enter\n For exit print 3 + enter\n";
+    cout<<main_menu;
+    int numr;
+    cin>> numr;
+    
+
+    return 0;
+}
